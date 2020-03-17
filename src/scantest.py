16eb3a358c1f251d6946b6457aa4fa0289a2b4f3
bedlamzd <bedlamzd@gmail.com>
2020-03-17 14:52:36 +0300

    added time measurement; added roi; added extraction_mode; added if __name__:... for easier profiling

--- src/scantest.py
+++ src/scantest.py
@@ -2,12 +2,14 @@ import Scaner
 import utilities
 import numpy as np
 import cv2
+import time
 
 fx = 580
 fy = fx
 u0 = 640 // 2 - 1
 v0 = 480 // 2 - 1
 roll, pitch, yaw = np.radians([0, 30, 0])
+roi = [30, 100,600, 300]
 
 mtx = np.array([[fx, 0, u0],
                 [0, fy, v0],
@@ -16,23 +18,28 @@ rot_mtx = utilities.roteul(pitch, yaw, roll, order='XYZ')
 R = np.array([[0, 0, -1],
               [-1, 0, 0],
               [0, 1, 0]])
-rot_mtx = R@rot_mtx
+rot_mtx = R @ rot_mtx
 tvec = np.array([200, 112, 152.27])
 
 height = tvec[2] / np.cos(pitch)
 angle = pitch
-velocity = 300/60
+velocity = 300 / 60
 img_proc_opts = {}
-extraction_opts = {}
+extraction_mode = 'ggm'
+extraction_opts = {'ksize': 29, 'sigma': 4.45}
 
-cap = cv2.VideoCapture(r"C:\Users\bedla\YandexDisk\MT.lab\МТ.П - Производство\МТ.П.001\Файлы для скана\scanner (2).mp4")
-camera = Scaner.Camera(mtx=mtx, rot_mtx=rot_mtx, tvec=tvec, cap=cap)
+cap = cv2.VideoCapture(r"C:\Users\bedla\YandexDisk\Диплом Борисов\Иллюстрации\Видео с камеры.mp4")
+camera = Scaner.Camera(mtx=mtx, rot_mtx=rot_mtx, tvec=tvec, cap=cap, roi=roi)
 scaner = Scaner.Scaner(camera=camera, height=height, angle=angle, velocity=velocity, img_proc_opts=img_proc_opts,
-                       extraction_opts=extraction_opts)
-scaner.scan()
-x, y, z = [x.reshape(x.shape[:2]) for x in np.dsplit(scaner.pointcloud,3)]
-xn, yn, zn = [utilities.normalize(x) for x in [x,y,z]]
-pointcloud = scaner.pointcloud
-np.clip(pointcloud[..., 2], 60, None, out=pointcloud[..., 2])
-utilities.show_height_map(pointcloud)
-# utilities.show_height_map(scaner.pointcloud)
+                       extraction_opts=extraction_opts, extraction_mode=extraction_mode)
+
+if __name__ == '__main__':
+    start = time.time()
+    scaner.scan()
+    print(time.time()-start)
+    pointcloud = scaner.pointcloud
+    # pointcloud[..., 2] = pointcloud[..., 2] - np.abs(pointcloud[:,0,2].reshape(pointcloud.shape[0], 1))
+    x, y, z = [x.reshape(x.shape[:2]) for x in np.dsplit(pointcloud,3)]
+    xn, yn, zn = [utilities.normalize(x) for x in [x,y,z]]
+    np.clip(pointcloud[..., 2], 0, None, out=pointcloud[..., 2])
+    # utilities.show_height_map(pointcloud)

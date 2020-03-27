c749e298f86e77c6a0261c35761f9e5eefc673ef
bedlamzd <bedlamzd@gmail.com>
2020-03-27 22:17:18 +0300

    rot_mtx and tvec from test

--- src/scantest.py
+++ src/scantest.py
@@ -11,17 +11,19 @@ fy = fx
 u0 = 640 // 2 - 1
 v0 = 480 // 2 - 1
 roll, pitch, yaw = np.radians([0, 30, 0])
-roi = [30, 100,600, 300]
+roi = [30, 100, 600, 300]
 
 mtx = np.array([[fx, 0, u0],
                 [0, fy, v0],
                 [0, 0, 1]])
-rot_mtx = roteul(pitch, yaw, roll, order='XYZ')
+rot_mtx = np.array([[0.99859376, 0.03779394, 0.03717441],
+                    [-0.01415901, 0.86591157, -0.49999658],
+                    [-0.05108658, 0.49876718, 0.86522901]])
 R = np.array([[0, 0, -1],
               [-1, 0, 0],
               [0, 1, 0]])
 rot_mtx = R @ rot_mtx
-tvec = np.array([200, 112, 152.27])
+tvec = np.array([-47.29274954, -17.4972774,  168.07156932])
 
 height = tvec[2] / np.cos(pitch)
 angle = pitch
@@ -38,10 +40,10 @@ scaner = Scanner.Scanner(camera=camera, height=height, angle=angle, velocity=vel
 if __name__ == '__main__':
     start = time.time()
     scaner.scan()
-    print(time.time()-start)
+    print(time.time() - start)
     pointcloud = scaner.pointcloud
     # pointcloud[..., 2] = pointcloud[..., 2] - np.abs(pointcloud[:,0,2].reshape(pointcloud.shape[0], 1))
-    x, y, z = [x.reshape(x.shape[:2]) for x in np.dsplit(pointcloud,3)]
-    xn, yn, zn = [normalize(x) for x in [x,y,z]]
+    x, y, z = [x.reshape(x.shape[:2]) for x in np.dsplit(pointcloud, 3)]
+    xn, yn, zn = [normalize(x) for x in [x, y, z]]
     np.clip(pointcloud[..., 2], 0, None, out=pointcloud[..., 2])
     # utilities.show_height_map(pointcloud)
